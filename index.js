4e5ddd859a757722f82cb477586fabf47485890a
mohammad <mohammad0914@gmail.com>
2022-08-16 13:17:34 +0430

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+let deckId;
+document.addEventListener('DOMContentLoaded', () => {
+    loadPage();
+    configureButtonClickEvent();
+}
+);
+function addRemainig(input) {
+    let remaining = document.querySelector("#remainigcards");
+    remaining.textContent = input;
+}
 
+function configureButtonClickEvent() {
+    let getCards = document.querySelector("#getCards");
+    getCards.addEventListener('click', () => {
+
+        if (deckId != null) {
+            let number = document.querySelector("#cardsnumber");
+            let numberOfCards = number.options[select.selectedIndex].value;
+            drawCards(deckId, parsInt(numberOfCards))
+        }
+    })
+}
+function loadPage() {
+    axios
+        .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(resposne => {
+            // debugger
+            if (resposne.data.success) {
+                deckId = resposne.data.deck_id;
+                addRemainig(resposne.data.remaining);
+                console.log(resposne.data);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+
+}
+
+function drawCards(id, number) {
+    debugger
+    let url = `https://deckofcardsapi.com/api/deck/${id}/draw/?count=${number}`;
+    axios
+        .get(url)
+        .then(response => {
+            response.data.cards.forEach(card => {
+                let imgElement = document.createElement("img");
+                imgElement.src = card["image"];
+                var section = document.querySelector("section");
+                section.appendChild(imgElement);
+            });
+
+
+
+        })
+        .catch(error => { console.log(error) });
+}
