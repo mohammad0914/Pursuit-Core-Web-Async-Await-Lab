9dcaf1674997191199f941f5fc0c107c74736819
mohammad <mohammad0914@gmail.com>
2022-08-17 08:59:09 +0430

    edit

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 );
 function addRemainig(input) {
-    let remaining = document.querySelector("#remainigcards");
+    let remaining = document.querySelector("#remaining");
     remaining.textContent = input;
 }
 
 function configureButtonClickEvent() {
     let getCards = document.querySelector("#getCards");
     getCards.addEventListener('click', () => {
-
+        // debugger
         if (deckId != null) {
             let number = document.querySelector("#cardsnumber");
-            let numberOfCards = number.options[select.selectedIndex].value;
-            drawCards(deckId, parsInt(numberOfCards))
+            let numberOfCards = number.options[number.selectedIndex].value;
+            drawCards(deckId, parseInt(numberOfCards))
         }
     })
 }
-function loadPage() {
-    axios
+async function loadPage() {
+    await axios
         .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(resposne => {
             // debugger
@@ -39,21 +39,33 @@ function loadPage() {
 
 }
 
-function drawCards(id, number) {
+async function drawCards(id, number) {
     debugger
     let url = `https://deckofcardsapi.com/api/deck/${id}/draw/?count=${number}`;
-    axios
+    await axios
         .get(url)
         .then(response => {
+            removeChild();
             response.data.cards.forEach(card => {
                 let imgElement = document.createElement("img");
                 imgElement.src = card["image"];
+                imgElement.className = ".card";
                 var section = document.querySelector("section");
                 section.appendChild(imgElement);
             });
 
-
-
+            let remain = response.data.remaining;
+            addRemainig(remain);
         })
         .catch(error => { console.log(error) });
 }
+
+function removeChild() {
+    var section = document.querySelector("section");
+    var images = document.querySelectorAll("img");
+    for (let image of images) {
+        section.removeChild(image);
+    }
+}
+
+
